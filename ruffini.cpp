53ed6c00a1244d669d440292cb94d667b2ec0157
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-12-29 11:09:28 +0100

    Branch Init

---
+++ ruffini.cpp
@@ -0,0 +1,8 @@
+#include "ruffini.h"
+#include <iostream>
+#include <map>
+#include <string>
+
+typedef std::map<std::string, int> dict_t;
+
+dict_t ruffini(float multi, int coefficients[]) {}
---
+++ ruffini.h
@@ -0,0 +1,1 @@
+double ruffini(float multi, int coefficients[]);
