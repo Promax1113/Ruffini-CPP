b0e47e16e67b73401178968b7ecb7816e9b4b5c8
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2024-01-08 14:34:09 +0100

    Fix type errors and testing

--- ruffini.cpp
+++ ruffini.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
-typedef std::map<std::string, int> dict_t;
+Ruffini::dict_t Ruffini::ruffini(float multi, std::vector<float> coefficients) {
+    std::vector<float> resultant;
+    std::vector<float> results = {coefficients[0]};
 
-dict_t ruffini(float multi, int coefficients[]) {}
+    for (unsigned i = 1; i < coefficients.size(); i++){
+        results.push_back((results.at(i - 1) * multi) + coefficients.at(i));
+    }
+
+    float remainder = results.at(results.size() - 1);
+    results.pop_back();
+    dict_t returnable = {remainder, results};
+
+    return returnable;
+}
--- ruffini.h
+++ ruffini.h
@@ -1,1 +1,17 @@
-double ruffini(float multi, int coefficients[]);
+
+#ifndef RUFFINI_H
+#define RUFFINI_H
+
+#include <map>
+#include <vector>
+#include <string>
+
+class Ruffini{
+    public:
+        typedef std::pair<float, std::vector<float>> dict_t;
+
+        static dict_t ruffini(float multi, std::vector<float> coefficients);
+};
+
+
+#endif // RUFFINI_H
